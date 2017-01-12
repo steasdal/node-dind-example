11416b0fffea94f57ce2f748343e81a7cf627803
Sam Teasdale <steasdale@appriss.com>
2017-01-12 10:03:10 -0500

    Added explanatory verbiage to build.sh, updated README

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # node-dind-example
 
-A super simple Node.js "hello world" app that can be built docker-in-docker style.
+This project contains a super simple Node.js "hello world" app that we'll build into a Docker image
+with a super nifty (and totally bonkers) docker-in-docker build.
 
-## Running Locally
+## Running the Node.js app locally
 
-Make sure you have [Node.js](http://nodejs.org/) installed.
-Run the following:
+If you'd like to experience the "hello world" awesomeness without running the Docker build, try
+the following:
 
-    npm install
-    npm start
+   * install Node.js
+   * run `npm install`
+   * run `npm start`
 
-Your app should now be running on [localhost:5000](http://localhost:5000/).
+The app should now be running at [localhost:5000](http://localhost:5000/).
+
+## Running the docker-in-docker build.
+
+The `build.sh` script is the real star of the show here.  Dive right in and have a look.  You'll need
+to have Docker installed to run it.  This script will setup a docker-in-docker environment, run the
+Docker build (e.g. process the Dockerfile) from within the "internal" Docker container, attempt to
+push the resulting image up to the official Docker hub and then clean up after itself.
+
+The build script will look for a Docker config file (config.json) in a ~/.docker directory.  If you don't
+have one, the script will display a warning but continue on.  You'll then also see warnings from the
+"internal" section of the build script when it attempts to push the freshly built image up to the official
+Docker hub.  Again, just ignore those warnings.
 
--- build.sh
+++ build.sh
@@ -7,29 +7,56 @@ if [ -z ${INSIDE_THE_DOCKER_CLIENT+x} ]; then
     # Create a unique identifier for this build based
     # on the first eleven characters of the git hash.
     GIT_HASH=$(git rev-parse --verify --short=11 HEAD)
-    echo "git hash: ${GIT_HASH}"
+    echo "Git hash: ${GIT_HASH}"
 
     # Capture a list of docker volumes before the build
     VOLUMES_BEFORE_BUILD_FILENAME=volumes-before-build-${GIT_HASH}.txt
     docker volume ls -q > ${VOLUMES_BEFORE_BUILD_FILENAME}
 
-    DOCKER_IMAGE_NAME=steasdal/dind-example
-    echo "docker image name: ${DOCKER_IMAGE_NAME}"
+    DOCKER_IMAGE_NAME=steasdal/node-dind-example
+    echo "Docker image name: ${DOCKER_IMAGE_NAME}"
 
     DOCKER_HOST=docker-host-${GIT_HASH}
-    echo "docker host: ${DOCKER_HOST}"
+    echo "Docker host: ${DOCKER_HOST}"
 
     DOCKER_CLIENT=docker-client-${GIT_HASH}
-    echo "docker client: ${DOCKER_CLIENT}"
+    echo "Docker client: ${DOCKER_CLIENT}"
 
     STORAGE_DRIVER=`docker info | grep 'Storage Driver:' | awk '{print $NF}'`
-    echo "storage driver: ${STORAGE_DRIVER}"
+    echo "Storage driver: ${STORAGE_DRIVER}"
 
+    # Spin up the DOCKER_HOST container that'll host the docker daemon for the DOCKER_CLIENT
+    # to use.  Note the "dind" tag.  Using the same storage driver as the host docker system
+    # guarantees best possible disk performance.  See the docker-in-docker documentation for
+    # more info:  https://hub.docker.com/_/docker/
     echo "Starting docker host ${DOCKER_HOST} with storage driver: ${STORAGE_DRIVER}"
     docker run --privileged --name ${DOCKER_HOST} -d docker:dind --storage-driver=${STORAGE_DRIVER}
 
+    # Ok, here we go.  This is where things really start to get fun.  Let's walk
+    # through the following gargantuan docker run command line by line, shall we?
+    #
+    # 1) Mount local docker config file with docker hub creds into the DOCKER_CLIENT container
+    #    so that the push to the official Docker hub works.
+    # 2) Create a tmp directory in the DOCKER_CLIENT container.
+    # 3) Set that tmp directory as the working directory.
+    # 4) Set the GIT_HASH environment variable in the DOCKER_CLIENT container.
+    # 5) Set the DOCKER_IMAGE_NAME environment variable in the DOCKER_CLIENT container
+    # 6) Set the INSIDE_THE_DOCKER_CLIENT environment variable in the internal docker container.
+    #    This is the trigger that tells this script to execute the INTERNAL section of itself.
+    # 7) Set a name for the DOCKER_CLIENT container.
+    # 8) Link the DOCKER_CLIENT container to DOCKER_HOST container that we just spun up.
+    #    The DOCKER_CLIENT container will use the docker daemon running in DOCKER_HOST since
+    #    it has no docker daemon itself.
+    # 9) Lastly, spin up the DOCKER_CLIENT container and tell it to execute this very build
+    #    script.  The $0 variable resolves to the name of this script.  Since we mounted $PWD
+    #    into the DOCKER_CLIENT container's working directtory, this script will be right
+    #    there and ready to go.  Again, because we've set the INSIDE_THE_DOCKER_CLIENT
+    #    environment variable on the DOCKER_CLIENT container, this script will execute its
+    #    internal section.
+
     echo "Starting build in docker client: ${DOCKER_CLIENT}"
-    docker run -v $PWD:/tmp \
+    docker run -v ~/.docker/config.json:/root/.docker/config.json \
+               -v $PWD:/tmp \
                -w /tmp \
                -e "GIT_HASH=${GIT_HASH}" \
                -e "DOCKER_IMAGE_NAME=${DOCKER_IMAGE_NAME}" \
@@ -58,17 +85,24 @@ else
 
     echo "*** Entering the INTERNAL section of build script $0 ***"
 
+    # If we're here, we're running in the internal docker container.  Dockerception, baby!
+    # Time to run the docker build.  Since we've mounted the contents of $PWD into this
+    # container's working directory, the Dockerfile should have come along for the ride.
+    # All of the environment variables that we're using below should have been injected
+    # into this container's context by the external docker run command.
+    #
+    # Let's kick off the build and tag it with two tags:  "latest" and $GIT_HASH
     echo "building ${DOCKER_IMAGE_NAME} image with 'latest' and '${GIT_HASH}' tags"
     docker build --no-cache=true --force-rm=true --pull \
                  -f Dockerfile \
                  -t ${DOCKER_IMAGE_NAME}:latest \
                  -t ${DOCKER_IMAGE_NAME}:${GIT_HASH} .
 
-    #echo "pushing ${DOCKER_IMAGE_NAME}:latest to docker repo "
-    #docker push ${DOCKER_IMAGE_NAME}:latest
+    echo "pushing ${DOCKER_IMAGE_NAME}:latest to docker repo "
+    docker push ${DOCKER_IMAGE_NAME}:latest
 
-    #echo "pushing ${DOCKER_IMAGE_NAME}:${GIT_HASH} to docker repo "
-    #docker push ${DOCKER_IMAGE_NAME}:${GIT_HASH}
+    echo "pushing ${DOCKER_IMAGE_NAME}:${GIT_HASH} to docker repo "
+    docker push ${DOCKER_IMAGE_NAME}:${GIT_HASH}
 
     echo "*** Exiting the INTERNAL section of build script $0 ***"
 
